f5ec28a92097a15d63daa00e0533b6949d26e524
Ermi <manermidem@gmail.com>
2023-04-04 18:57:42 +0300

    Add join table for medical_history and treatments

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,16 +1,52 @@
 CREATE DATABASE schema_diagram;
 
 
- CREATE TABLE patients(id INT PRIMARY KEY, name VARCHAR, date_of_birth DATE);
+ CREATE TABLE patients(
+    id INT PRIMARY KEY GENERATED ALWAYS ON IDENTITY, 
+    name VARCHAR(120), 
+    date_of_birth DATE
+);
 
- CREATE TABLE medical_histories(id INT PRIMARY KEY, admitted_at TIMESTAMP, patient_id INT, status VARCHAR, FOREIGN KEY (patient_id) REFERENCES patients(id));
+ CREATE TABLE medical_histories(
+    id INT PRIMARY KEY GENERATED ALWAYS ON IDENTITY, 
+    admitted_at TIMESTAMP, 
+    patient_id INT, 
+    status VARCHAR, 
+    FOREIGN KEY (patient_id) REFERENCES patients (id) ON DELETE CASCADE
+    );
 
- CREATE TABLE treatments(id INT PRIMARY KEY, type VARCHAR, name VARCHAR);
+ CREATE TABLE treatments(
+    id INT PRIMARY KEY GENERATED ALWAYS ON IDENTITY, 
+    type VARCHAR, 
+    name VARCHAR
+    );
 
-
+CREATE TABLE medical_histories_has_treatments(
+    id PRIMARY KEY GENERATED ALWAYS ON IDENTITY,
+    medical_histories_id INT NOT NULL,
+    treatments_id INT NOT NULL,
+    FOREIGN KEY (medical_histories_id) REFERENCES medical_histories (id) ON DELETE CASCADE
+    FOREIGN KEY (treatments_id) REFERENCES treatments (id) ON DELETE CASCADE
+);
 
 SELECT * FROM patients;
 
-CREATE TABLE invoices(id INT PRIMARY KEY, total_amount INT, generated_at TIMESTAMP, payed_at TIMESTAMP, medical_history_id INT, FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id));
+CREATE TABLE invoices(
+    id INT PRIMARY KEY, 
+    total_amount INT, 
+    generated_at TIMESTAMP, 
+    payed_at TIMESTAMP, 
+    medical_history_id INT, 
+    FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id) ON DELETE CASCADE
+    );
 
-CREATE TABLE invoice_items(id INT PRIMARY KEY,unit_price DECIMAL,quantity INT, total_price DECIMAL, invoice_id INT, treatment_id INT, FOREIGN KEY (treatment_id) REFERENCES treatments(id) , FOREIGN KEY (invoice_id) REFERENCES invoices(id));
+CREATE TABLE invoice_items(
+    id INT PRIMARY KEY,
+    unit_price DECIMAL,
+    quantity INT, 
+    total_price DECIMAL, 
+    invoice_id INT, 
+    treatment_id INT, 
+    FOREIGN KEY (treatment_id) REFERENCES treatments (id) , 
+    FOREIGN KEY (invoice_id) REFERENCES invoices(id) ON DELETE CASCADE
+    );
