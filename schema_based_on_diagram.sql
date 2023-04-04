ba99edf05c91e884977b0c306f8b4a925b0bc142
Ermi <manermidem@gmail.com>
2023-04-04 17:44:37 +0300

    Create tables for the invoices and invoice_items

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -10,3 +10,7 @@ CREATE DATABASE schema_diagram;
 
 
 SELECT * FROM patients;
+
+CREATE TABLE invoices(id INT PRIMARY KEY, total_amount INT, generated_at TIMESTAMP, payed_at TIMESTAMP, medical_history_id INT, FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id));
+
+CREATE TABLE invoice_items(id INT PRIMARY KEY,unit_price DECIMAL,quantity INT, total_price DECIMAL, invoice_id INT, treatment_id INT, FOREIGN KEY (treatment_id) REFERENCES treatments(id) , FOREIGN KEY (invoice_id) REFERENCES invoices(id));
